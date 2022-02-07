4585563a96e7180fc1f7876395bdfe30a4660112
omukoyiaquinter <97959493+omukoyiaquinter@users.noreply.github.com>
2022-02-07 17:11:29 +0300

    COUNTY

---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+County Library system
+by Quinter Omukoyia
+Feb 2022
+MIT License
+C99 Compiler
+*/
+{
+    printf("select an action below\n")
+    print("1.Add new Patron\n")
+    print("2.view all resources\n")
+}print("3.add new resources\n")
+int menu
+    printf("select an action below\n")
+    print("1.Add new Patron\n")
+    print("2.view all resources\n")
+}print("3.add new resources\n")
+printf("Your action\n")
+scanf("%d",&action)
+return action
+
+int main
+    printf("COUNTY LIBRARY SYSTEM\n")
+    print("welcome Mr Quinter\n")
+    print("2.view all resources\n")
+
+
+
+
+
+int main()
+
+int action
+printf
